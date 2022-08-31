c2ca2bf7c6f700c4f92999326b8c60879d6e31d5
BII KIPKOECH COLLINS <ckipkoech17@gmail.com>
2022-08-31 13:35:12 +0300

    commit tree 014f776fe33bfa51c76c28ad39bb81fa18dbc08e

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,20 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    self.songs.first.genre
+    # artist=Artist.first
+    # artist.genres
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
+    # self.song.where(genre==self.genre)
+
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    self.genres.count
   end
 end
